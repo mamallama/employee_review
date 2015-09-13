f35db0006daea7e3020d0293a9f06e8d1ecc602f
Llamamama <reneeq25@gmail.com>
2015-09-12 23:23:49 -0400

    Making it fun. Harry Potter introduced

--- employee_reviews_test.rb
+++ employee_reviews_test.rb
@@ -15,14 +15,21 @@ class EmployeeReviewsTest < Minitest::Test
   end
 
   def test_department_name
-  assert_equal "Marketing", Department.new("Marketing").dept
+  assert_equal "Auror Office", Department.new("Auror Office").dept #Decided to go with HP theme
   end
 
   def test_create_employee #going one by one, testing for required info
-    employee = Employee.new(name: "Abbey", email: "ab@mail.com", phone: "2345676", salary: 38000)
-    assert_equal "Abbey", employee.name
-    assert_equal "ab@mail.com", employee.email
+    employee = Employee.new(name: "Harry", email: "hp@mail.com", phone: "2345676", salary: 38000)
+    assert_equal "Harry", employee.name
+    assert_equal "hp@mail.com", employee.email
     assert_equal "2345676", employee.phone
     assert_equal 38000, employee.salary
   end
+
+  def test_add_employee_to_dept
+    employee = Employee.new("Hermione")
+    dept = Department.new("SPEW")
+    assert_equal #employee is in dept. HOW? block... .select?
+  end
+
 end
