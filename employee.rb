1271af2bb13f6cb68bf63b42416a042943af4af8
Llamamama <reneeq25@gmail.com>
2015-09-12 23:09:25 -0400

    Creates new employee

--- employee.rb
+++ employee.rb
@@ -1,13 +1,11 @@
 
 class Employee
-  attr_reader :name
-  
-  def initialize(name)
-    @name = name
-  end
+  attr_reader :name, :email, :phone, :salary
 
-  def salary
+  def initialize(name:, email: "", phone: "", salary: 0)
+    @name = name
+    @email = email
+    @phone = phone
     @salary = salary
   end
-
 end
--- employee_reviews_test.rb
+++ employee_reviews_test.rb
@@ -15,6 +15,14 @@ class EmployeeReviewsTest < Minitest::Test
   end
 
   def test_department_name
-  assert_equal "Marketing", Department.new("Marketing").dept 
+  assert_equal "Marketing", Department.new("Marketing").dept
+  end
+
+  def test_create_employee #going one by one, testing for required info
+    employee = Employee.new(name: "Abbey", email: "ab@mail.com", phone: "2345676", salary: 38000)
+    assert_equal "Abbey", employee.name
+    assert_equal "ab@mail.com", employee.email
+    assert_equal "2345676", employee.phone
+    assert_equal 38000, employee.salary
   end
 end
