4f9da3e3628a4fddf2084a6232801d4583c713b6
Llamamama <reneeq25@gmail.com>
2015-09-12 22:50:32 -0400

    Department can be named

--- employee_reviews_test.rb
+++ employee_reviews_test.rb
@@ -13,4 +13,8 @@ class EmployeeReviewsTest < Minitest::Test
   def test_dept_creation
     assert Department.new("New Department")
   end
+
+  def test_department_name
+  assert_equal "Marketing", Department.new("Marketing").dept 
+  end
 end
